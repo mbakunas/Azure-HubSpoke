bb71a48fb3304a566066c737b180084390019d95
mbakunas <mbakunas@users.noreply.github.com>
2020-12-24 21:35:28 -0500

    Moved function to the end of the script

--- PowerShell/New-DeliverableData.ps1
+++ PowerShell/New-DeliverableData.ps1
@@ -1,59 +1,3 @@
-function Get-IPrangeStartEnd { 
-    <#  
-      .SYNOPSIS   
-        Get the IP addresses in a range  
-      .EXAMPLE  
-       Get-IPrangeStartEnd -start 192.168.8.2 -end 192.168.8.20  
-      .EXAMPLE  
-       Get-IPrangeStartEnd -ip 192.168.8.2 -mask 255.255.255.0  
-      .EXAMPLE  
-       Get-IPrangeStartEnd -ip 192.168.8.3 -cidr 24  
-    #>  
-      
-    param (  
-        [string]$start,  
-        [string]$end,  
-        [string]$ip,  
-        [string]$mask,  
-        [int]$cidr  
-    )  
-      
-    function IP-toINT64 () {  
-        param ($ip)  
-      
-        $octets = $ip.split(".")  
-        return [int64]([int64]$octets[0] * 16777216 + [int64]$octets[1] * 65536 + [int64]$octets[2] * 256 + [int64]$octets[3])  
-    }  
-      
-    function INT64-toIP() {  
-        param ([int64]$int)  
- 
-        return (([math]::truncate($int / 16777216)).tostring() + "." + ([math]::truncate(($int % 16777216) / 65536)).tostring() + "." + ([math]::truncate(($int % 65536) / 256)).tostring() + "." + ([math]::truncate($int % 256)).tostring() ) 
-    }  
-      
-    if ($ip) { $ipaddr = [Net.IPAddress]::Parse($ip) }  
-    if ($cidr) { $maskaddr = [Net.IPAddress]::Parse((INT64-toIP -int ([convert]::ToInt64(("1" * $cidr + "0" * (32 - $cidr)), 2)))) }  
-    if ($mask) { $maskaddr = [Net.IPAddress]::Parse($mask) }  
-    if ($ip) { $networkaddr = new-object net.ipaddress ($maskaddr.address -band $ipaddr.address) }  
-    if ($ip) { $broadcastaddr = new-object net.ipaddress (([system.net.ipaddress]::parse("255.255.255.255").address -bxor $maskaddr.address -bor $networkaddr.address)) }  
-      
-    if ($ip) {  
-        $startaddr = IP-toINT64 -ip $networkaddr.ipaddresstostring  
-        $endaddr = IP-toINT64 -ip $broadcastaddr.ipaddresstostring  
-    }
-    else {  
-        $startaddr = IP-toINT64 -ip $start  
-        $endaddr = IP-toINT64 -ip $end  
-    }  
-      
-    #$temp=""|Select start,end 
-    #$temp.start=INT64-toIP -int $startaddr 
-    #$temp.end=INT64-toIP -int $endaddr
-    $tempStart = INT64-toIP -int $startaddr
-    $tempEnd = INT64-toIP -int $endaddr
-    return "$tempStart - $tempEnd"
-}
-
 $parametersFile = "$env:USERPROFILE\Documents\GitHub\Azure-HubSpoke\Archived\JSON.json"
 $deliverableDataFilePath = "$env:USERPROFILE\Documents\GitHub\Azure-HubSpoke\PowerShell\deliverableData.csv"
 
@@ -224,3 +168,58 @@ for ($i = 1; $i -lt $output.Count; $i++) {
 # write output to file
 
 $csvOutput | Export-Csv -Path $deliverableDataFilePath -NoTypeInformation
+function Get-IPrangeStartEnd { 
+    <#  
+      .SYNOPSIS   
+        Get the IP addresses in a range  
+      .EXAMPLE  
+       Get-IPrangeStartEnd -start 192.168.8.2 -end 192.168.8.20  
+      .EXAMPLE  
+       Get-IPrangeStartEnd -ip 192.168.8.2 -mask 255.255.255.0  
+      .EXAMPLE  
+       Get-IPrangeStartEnd -ip 192.168.8.3 -cidr 24  
+    #>  
+      
+    param (  
+        [string]$start,  
+        [string]$end,  
+        [string]$ip,  
+        [string]$mask,  
+        [int]$cidr  
+    )  
+      
+    function IP-toINT64 () {  
+        param ($ip)  
+      
+        $octets = $ip.split(".")  
+        return [int64]([int64]$octets[0] * 16777216 + [int64]$octets[1] * 65536 + [int64]$octets[2] * 256 + [int64]$octets[3])  
+    }  
+      
+    function INT64-toIP() {  
+        param ([int64]$int)  
+ 
+        return (([math]::truncate($int / 16777216)).tostring() + "." + ([math]::truncate(($int % 16777216) / 65536)).tostring() + "." + ([math]::truncate(($int % 65536) / 256)).tostring() + "." + ([math]::truncate($int % 256)).tostring() ) 
+    }  
+      
+    if ($ip) { $ipaddr = [Net.IPAddress]::Parse($ip) }  
+    if ($cidr) { $maskaddr = [Net.IPAddress]::Parse((INT64-toIP -int ([convert]::ToInt64(("1" * $cidr + "0" * (32 - $cidr)), 2)))) }  
+    if ($mask) { $maskaddr = [Net.IPAddress]::Parse($mask) }  
+    if ($ip) { $networkaddr = new-object net.ipaddress ($maskaddr.address -band $ipaddr.address) }  
+    if ($ip) { $broadcastaddr = new-object net.ipaddress (([system.net.ipaddress]::parse("255.255.255.255").address -bxor $maskaddr.address -bor $networkaddr.address)) }  
+      
+    if ($ip) {  
+        $startaddr = IP-toINT64 -ip $networkaddr.ipaddresstostring  
+        $endaddr = IP-toINT64 -ip $broadcastaddr.ipaddresstostring  
+    }
+    else {  
+        $startaddr = IP-toINT64 -ip $start  
+        $endaddr = IP-toINT64 -ip $end  
+    }  
+      
+    #$temp=""|Select start,end 
+    #$temp.start=INT64-toIP -int $startaddr 
+    #$temp.end=INT64-toIP -int $endaddr
+    $tempStart = INT64-toIP -int $startaddr
+    $tempEnd = INT64-toIP -int $endaddr
+    return "$tempStart - $tempEnd"
+}
